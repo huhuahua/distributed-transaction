752c4d4205b06bffad91310f53499d861fcc8c3b
huhua <949953387@qq.com>
2020-07-10 09:26:28 +0800

    分布式事务示例

---
+++ order-service/src/main/java/org/zl/transaction/order/service/IMessageService.java
@@ -0,0 +1,16 @@
+package org.zl.transaction.order.service;
+
+import org.zl.transaction.order.entity.Message;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 事务表 服务类
+ * </p>
+ *
+ * @author zl
+ * @since 2018-12-13
+ */
+public interface IMessageService extends IService<Message> {
+
+}
